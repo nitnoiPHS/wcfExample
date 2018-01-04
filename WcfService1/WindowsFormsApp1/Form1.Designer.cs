d2e30b06b049c00ba7ca9aea17b32d7724101051
DESKTOP-ITNTJPA\PrisanaPHS <prisana.phs@gmail.com>
2018-01-04 22:09:20 +0700

    Add interface

---
+++ WcfService1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.val1 = new System.Windows.Forms.TextBox();
+            this.val2 = new System.Windows.Forms.TextBox();
+            this.re1 = new System.Windows.Forms.TextBox();
+            this.val5 = new System.Windows.Forms.TextBox();
+            this.re2 = new System.Windows.Forms.TextBox();
+            this.val4 = new System.Windows.Forms.TextBox();
+            this.val3 = new System.Windows.Forms.TextBox();
+            this.val7 = new System.Windows.Forms.TextBox();
+            this.re3 = new System.Windows.Forms.TextBox();
+            this.val6 = new System.Windows.Forms.TextBox();
+            this.re4 = new System.Windows.Forms.TextBox();
+            this.val8 = new System.Windows.Forms.TextBox();
+            this.sum = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "+";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "/";
+            // 
+            // val1
+            // 
+            this.val1.Location = new System.Drawing.Point(53, 52);
+            this.val1.Name = "val1";
+            this.val1.Size = new System.Drawing.Size(37, 20);
+            this.val1.TabIndex = 4;
+            // 
+            // val2
+            // 
+            this.val2.Location = new System.Drawing.Point(115, 52);
+            this.val2.Name = "val2";
+            this.val2.Size = new System.Drawing.Size(37, 20);
+            this.val2.TabIndex = 5;
+            // 
+            // re1
+            // 
+            this.re1.Location = new System.Drawing.Point(247, 52);
+            this.re1.Name = "re1";
+            this.re1.Size = new System.Drawing.Size(37, 20);
+            this.re1.TabIndex = 6;
+            // 
+            // val5
+            // 
+            this.val5.Location = new System.Drawing.Point(53, 128);
+            this.val5.Name = "val5";
+            this.val5.Size = new System.Drawing.Size(37, 20);
+            this.val5.TabIndex = 7;
+            // 
+            // re2
+            // 
+            this.re2.Location = new System.Drawing.Point(247, 86);
+            this.re2.Name = "re2";
+            this.re2.Size = new System.Drawing.Size(37, 20);
+            this.re2.TabIndex = 8;
+            // 
+            // val4
+            // 
+            this.val4.Location = new System.Drawing.Point(115, 90);
+            this.val4.Name = "val4";
+            this.val4.Size = new System.Drawing.Size(37, 20);
+            this.val4.TabIndex = 9;
+            // 
+            // val3
+            // 
+            this.val3.Location = new System.Drawing.Point(54, 90);
+            this.val3.Name = "val3";
+            this.val3.Size = new System.Drawing.Size(37, 20);
+            this.val3.TabIndex = 10;
+            // 
+            // val7
+            // 
+            this.val7.Location = new System.Drawing.Point(54, 160);
+            this.val7.Name = "val7";
+            this.val7.Size = new System.Drawing.Size(37, 20);
+            this.val7.TabIndex = 11;
+            // 
+            // re3
+            // 
+            this.re3.Location = new System.Drawing.Point(247, 128);
+            this.re3.Name = "re3";
+            this.re3.Size = new System.Drawing.Size(37, 20);
+            this.re3.TabIndex = 12;
+            // 
+            // val6
+            // 
+            this.val6.Location = new System.Drawing.Point(115, 128);
+            this.val6.Name = "val6";
+            this.val6.Size = new System.Drawing.Size(37, 20);
+            this.val6.TabIndex = 13;
+            // 
+            // re4
+            // 
+            this.re4.Location = new System.Drawing.Point(247, 160);
+            this.re4.Name = "re4";
+            this.re4.Size = new System.Drawing.Size(37, 20);
+            this.re4.TabIndex = 14;
+            // 
+            // val8
+            // 
+            this.val8.Location = new System.Drawing.Point(115, 160);
+            this.val8.Name = "val8";
+            this.val8.Size = new System.Drawing.Size(37, 20);
+            this.val8.TabIndex = 15;
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(182, 50);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(43, 23);
+            this.sum.TabIndex = 16;
+            this.sum.Text = "=";
+            this.sum.UseVisualStyleBackColor = true;
+            this.sum.Click += new System.EventHandler(this.sum_Click);
+            // 
+            // div
+            // 
+            this.div.Location = new System.Drawing.Point(182, 158);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(43, 23);
+            this.div.TabIndex = 17;
+            this.div.Text = "=";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // mul
+            // 
+            this.mul.Location = new System.Drawing.Point(182, 126);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(43, 23);
+            this.mul.TabIndex = 18;
+            this.mul.Text = "=";
+            this.mul.UseVisualStyleBackColor = true;
+            this.mul.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(182, 88);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(43, 23);
+            this.sub.TabIndex = 19;
+            this.sub.Text = "=";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "ใส่ค่าในช่อง แล้วกด = เพื่อดูผลลัพธ์";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 274);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.val8);
+            this.Controls.Add(this.re4);
+            this.Controls.Add(this.val6);
+            this.Controls.Add(this.re3);
+            this.Controls.Add(this.val7);
+            this.Controls.Add(this.val3);
+            this.Controls.Add(this.val4);
+            this.Controls.Add(this.re2);
+            this.Controls.Add(this.val5);
+            this.Controls.Add(this.re1);
+            this.Controls.Add(this.val2);
+            this.Controls.Add(this.val1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox val1;
+        private System.Windows.Forms.TextBox val2;
+        private System.Windows.Forms.TextBox re1;
+        private System.Windows.Forms.TextBox val5;
+        private System.Windows.Forms.TextBox re2;
+        private System.Windows.Forms.TextBox val4;
+        private System.Windows.Forms.TextBox val3;
+        private System.Windows.Forms.TextBox val7;
+        private System.Windows.Forms.TextBox re3;
+        private System.Windows.Forms.TextBox val6;
+        private System.Windows.Forms.TextBox re4;
+        private System.Windows.Forms.TextBox val8;
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WcfService1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sum_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new
+                ServiceReference1.Service1Client();
+            // string returnString;
+
+            int result;
+            int value1 = Convert.ToInt32(val1.Text);
+            int value2 = Convert.ToInt32(val2.Text);
+            result = client.sum(value1, value2);
+
+            re1.Text = Convert.ToString(result);
+        }
+
+        private void sub_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new
+               ServiceReference1.Service1Client();
+            // string returnString;
+
+            int result;
+            int value1 = Convert.ToInt32(val3.Text);
+            int value2 = Convert.ToInt32(val4.Text);
+            result = client.Subtract(value1, value2);
+
+            re2.Text = Convert.ToString(result);
+        }
+
+        private void mul_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new
+               ServiceReference1.Service1Client();
+            // string returnString;
+
+            int result;
+            int value1 = Convert.ToInt32(val5.Text);
+            int value2 = Convert.ToInt32(val6.Text);
+            result = client.Multiply(value1, value2);
+
+            re3.Text = Convert.ToString(result);
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client client = new
+               ServiceReference1.Service1Client();
+            // string returnString;
+
+            int result;
+            int value1 = Convert.ToInt32(val7.Text);
+            int value2 = Convert.ToInt32(val8.Text);
+            result = client.Divide(value1, value2);
+
+            re4.Text = Convert.ToString(result);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
